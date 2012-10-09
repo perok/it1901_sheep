b18cc7b453c3ecd30707189ce97bcc353b562915
unknown <larserikgk@gmail.com>
2012-10-09 14:07:13 +0200

    Minor change response

--- project_inputsim/src/com/net/Response.java
+++ project_inputsim/src/com/net/Response.java
@@ -1,16 +1,15 @@
 package com.net;
 
 import java.io.*;
+import java.util.ArrayList;
 /*
  * This class defines the different type of messages that will be exchanged between the
  * Clients and the Server. 
  * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no 
  * need to count bytes or to wait for a line feed at the end of the frame
  */
-public class Response implements Serializable {
-
+public class Response implements Serializable { 
 	protected static final long serialVersionUID = 2;
-
 	// The different types of message sent by the Client
 	// WHOISIN to receive the list of the users connected
 	// MESSAGE an ordinary message
@@ -18,6 +17,7 @@ public class Response implements Serializable {
 	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
 	private int type;
 	private String message;
+	private ArrayList<String> content;
 	
 	// constructor
 	Response(int type, String message) {
@@ -26,11 +26,23 @@ public class Response implements Serializable {
 	}
 	
 	// getters
-	int getType() {
+	public int getType() {
 		return type;
 	}
-	String getMessage() {
+	public String getMessage() {
 		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public ArrayList<String> getContent() {
+		return content;
+	}
+
+	public void setContent(ArrayList<String> content) {
+		this.content = content;
 	}
 }
 
