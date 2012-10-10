29eca4e89e53829e8c20428c3ab6689232d5cb23
Tornaldo <tor.barstad@hotmail.com>
2012-10-10 13:10:14 +0200

    Litt nytt, men ikke noe spennende.

--- project/src/core/classes/Farm.java
+++ project/src/core/classes/Farm.java
@@ -2,6 +2,7 @@ package core.classes;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class Farm implements Comparator<Sheep> {
@@ -12,10 +13,12 @@ public class Farm implements Comparator<Sheep> {
 	
 	private ArrayList<Sheep> sheepList;
 	
+	boolean sammeOmigjen;
+	
 	int sorterEtter = 1;
 	/*Variabelen sorterEtter bestemmer om vi skal sortere etter default settings(1),
-	 * navn(2), alder(3) eller id(4). Litt uproff måte å gjøre det på, men det er ihvertfall
-	 * mine foreløpige løsning. -Tor
+	 * navn(2), alder(3) eller id(4). Litt uproff måte å gjøre det på kanskje, men det er
+	 * ihvertfall mine foreløpige løsning. -Tor
 	 */
 	
 	public boolean addSheep(Sheep sheep){
@@ -23,31 +26,54 @@ public class Farm implements Comparator<Sheep> {
 	}
 	
 	public ArrayList<Sheep> getDefaultSheepList(){
-		sorterEtter = 1;
+		if(sorterEtter == 1){
+			sammeOmigjen = true;
+		}
+		else{
+			sorterEtter = 1;
+		}
 		//Vet ikke om man burde sortere her i det hele tatt. Hva tenker dere?
 		return sheepList;
 	}
 	
 	public ArrayList<Sheep> getSheepByName(){
-		sorterEtter = 2;
-		
+		if(sorterEtter == 2){
+			sammeOmigjen = true;
+		}
+		else{
+			sorterEtter = 2;
+		}
+		Collections.sort(sheepList, this);
 		return sheepList;		
 	}
 
 	public ArrayList<Sheep> getSheepByAge(){
-		sorterEtter = 3;
+		if(sorterEtter == 3){
+			sammeOmigjen = true;
+		}
+		else{
+			sorterEtter = 3;
+		}
+		Collections.sort(sheepList, this);
 		return sheepList;		
 	}
 	
 	public ArrayList<Sheep> getSheepById(){
-		sorterEtter = 4;
+		if(sorterEtter == 1){
+			sammeOmigjen = true;
+		}
+		else{
+			sorterEtter = 4;
+		}
+		Collections.sort(sheepList, this);
 		return sheepList;		
 	}
 
 
 	public int compare(Sheep sheep1, Sheep sheep2) {
 		if(sorterEtter == 1){
-			
+			//Vet ikke hva man eventuelt burde sortere etter her.
+			return 0;
 		}
 		else if(sorterEtter == 2){
 			return sheep1.name.compareTo(sheep2.name);
@@ -56,7 +82,7 @@ public class Farm implements Comparator<Sheep> {
 			return sheep1.age - sheep2.age;
 		}
 		else if(sorterEtter == 4){
-			return sheep
+			return sheep1.id - sheep2.id;
 		}
 		return 0;
 	}
