8271bb140ac599c08108af0d0b6fe31daaa544ce
Per Øyvind <perok@stud.ntnu.no>
2012-10-23 19:52:13 +0200

    Basic table implemented

--- project/src/com/gui/StatisticsWidget.java
+++ project/src/com/gui/StatisticsWidget.java
@@ -2,6 +2,8 @@ package com.gui;
 
 import com.trolltech.qt.gui.QHBoxLayout;
 import com.trolltech.qt.gui.QPushButton;
+import com.trolltech.qt.gui.QTableWidget;
+import com.trolltech.qt.gui.QTableWidgetItem;
 import com.trolltech.qt.gui.QWidget;
 
 /** Hold statistics
@@ -15,6 +17,9 @@ public class StatisticsWidget extends QWidget
 	private QWidget qwStatWidget;
 	QPushButton btnOk = new QPushButton(tr("ok"));
 	
+	QTableWidget table = new QTableWidget(1000, 5);
+	QTableWidgetItem item = null;
+	
 	/** Constructor. Initialize..
 	 */
 	public StatisticsWidget()
@@ -27,8 +32,21 @@ public class StatisticsWidget extends QWidget
 	
 	private void poop()
 	{
-		btnOk.setMinimumHeight(20);
-		qhbMainLayout.addWidget(btnOk);
+		//btnOk.setMinimumHeight(20);
+		//qhbMainLayout.addWidget(btnOk);
+		
+		table.setMinimumHeight(20);
+		qhbMainLayout.addWidget(table);
+		//table.
+		
+		for(int i = 0; i < 1000; i++) {
+			 	for(int j = 0; j < 5; j++) {
+			      item = new QTableWidgetItem("Test value");
+			      table.setItem(i, j, item);
+			   }
+			}
+		
+		
 		
 		super.setLayout(qhbMainLayout);		
 	}
