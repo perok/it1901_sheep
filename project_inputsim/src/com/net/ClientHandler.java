0d821b8c1541a08c8b89ead7eaf28853470abf83
Lars Erik <larserikgk@gmail.com>
2012-11-08 01:11:25 +0100

    Fixed typos in handleresponse@CH

--- project_inputsim/src/com/net/ClientHandler.java
+++ project_inputsim/src/com/net/ClientHandler.java
@@ -101,7 +101,7 @@ public class ClientHandler implements Runnable {
 			System.out.println(lol.getUser().getName());
 			return lol;
 		case("editSheep"):
-			return new Response(Response.BOOLEAN,db.editUser(request.getUser().getId(), request.getUser()));
+			return new Response(Response.BOOLEAN,db.editSheep(request.getSheep().getId(), request.getSheep()));
 
 		case("editUser"):
 			return new Response(Response.BOOLEAN,db.editSheep(request.getSheep().getId(), request.getSheep()));
