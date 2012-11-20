c59e056eeab8de8e937771a8afda7bb02ea95195
Per Øyvind <perok@stud.ntnu.no>
2012-11-21 00:24:25 +0100

    Map regression

--- project/web/js/gmaps.js
+++ project/web/js/gmaps.js
@@ -14,6 +14,7 @@
 		center: trondheimLoc,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		streetViewControl: false,
+		disableDefaultUI: true,
 		mapTypeControlOptions: {
 			mapTypeIds: ['statkart', google.maps.MapTypeId.ROADMAP],
 			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
@@ -24,21 +25,6 @@
 	wmsType = new google.maps.ImageMapType(StatKartLayer);
 	 
 	map.mapTypes.set('statkart', wmsType);
-	
-
-	  alert("hey")
-	  if(navigator.geolocation) {
-		  alert("cake")
-          navigator.geolocation.getCurrentPosition(function(position) {
-              alert("yo");
-            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map.setCenter(pos);
-          });
-	  }else{
-		  alert("Fuck")
-	  }
-  
-  //makeLines(map);
 }
   
 
@@ -138,9 +124,9 @@ var shape = {
 var infowindow = new google.maps.InfoWindow({content:'hey'});
 
 var lineSymbol = {
-		path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
-		//strokeWeight: 3,
-		//strokeOpacity: 0.7
+		path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+		strokeWeight: 2,
+		strokeOpacity: 0.8
 	};
 
 //Add markers
